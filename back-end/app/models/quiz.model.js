0d85c4dd9e21ea92049bcb45b9838ae493c51423
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2022-03-22 09:44:22 +0100

    modif models non effective

--- back-end/app/models/quiz.model.js
+++ back-end/app/models/quiz.model.js
@@ -4,4 +4,7 @@ const BaseModel = require('../utils/base-model.js')
 module.exports = new BaseModel('Quiz', {
   theme: Joi.string().required(),
   name: Joi.string().required(),
+  // alzheimerOk: Joi.boolean.required(),
+  // ceciteOk: Joi.boolean.required(),
+  // cataracteOk: Joi.boolean.required(),
 })
