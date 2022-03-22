a510d681097c53fc3262fb58dabc12f377b80d3b
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2022-03-22 10:26:32 +0100

    Ajout de la page Settings pour Cataracte #2

---
+++ front-end/src/app/game/settings/game-setting/choice/choice-component/choice-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choice-component',
+  templateUrl: './choice-component.component.html',
+  styleUrls: ['./choice-component.component.scss']
+})
+export class ChoiceComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
