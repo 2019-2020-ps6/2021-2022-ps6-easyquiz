fe69ab90914e0fbc586d9417ddcb600a340db1cd
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2022-04-19 08:22:44 +0200

    Changement de config couleur par defaut

--- front-end/src/app/game/settings/game-setting/choice/choice-component/choice-component.component.ts
+++ front-end/src/app/game/settings/game-setting/choice/choice-component/choice-component.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Choice} from '../models/choice.model';
 import {SettingService} from '../../../../../../services/setting.service';
 import {Router} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-choice-component',
@@ -12,7 +13,7 @@ export class ChoiceComponentComponent implements OnInit {
 
   @Input() choice!: Choice;
 
-  constructor(private settingService: SettingService, private router: Router) {
+  constructor(private settingService: SettingService, private router: Router, private _location: Location) {
   }
 
   ngOnInit(): void {
@@ -21,7 +22,7 @@ export class ChoiceComponentComponent implements OnInit {
   onChoisirClick(): void {
     this.settingService.choiceSelected = this.choice;
     this.choice.buttonColor = 'red';
-    this.router.navigate(['/game/']);
+    this._location.back();
   }
 
 }
