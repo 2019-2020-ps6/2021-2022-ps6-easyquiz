6c9ea2614ca0bff597be8e39dde5dd25e1b0ea48
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2022-04-28 23:18:21 +0200

    choix theme dans liste debug#27
    marche aussi après raffraichissement

--- front-end/src/app/quizzes/quiz-form/quiz-form.component.ts
+++ front-end/src/app/quizzes/quiz-form/quiz-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { QuizService } from '../../../services/quiz.service';
@@ -14,17 +14,16 @@ export class QuizFormComponent implements OnInit {
   // Note: We are using here ReactiveForms to create our form. Be careful when you look for some documentation to
   // avoid TemplateDrivenForm (another type of form)
 
+  @Input()
+  themeList: string[];
+
   /**
    * QuizForm: Object which manages the form in our component.
    * More information about Reactive Forms: https://angular.io/guide/reactive-forms#step-1-creating-a-formgroup-instance
    */
   public quizForm: FormGroup;
 
-  public themeList: string[];
-
   constructor(public formBuilder: FormBuilder, public quizService: QuizService) {
-    this.themeList = quizService.getThemes();
-    console.log('first theme: ' + this.themeList[0]);
     this.quizForm = this.formBuilder.group({
       name: [''],
       theme: [''],
